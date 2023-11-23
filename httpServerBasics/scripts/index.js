2de54c7eaeabf79ec399659597137bf15ac2325a
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2023-11-23 14:29:10 +0545

    move js to scripts folder

---
+++ httpServerBasics/scripts/index.js
@@ -0,0 +1,26 @@
+
+
+function fillUserDetails(users) {
+    return users.map((user) => {
+        return `
+        <tr>
+            <td> ${user.name} </td>
+            <td> ${user.createdAt} </td>
+        </tr>
+        `;
+    });
+}
+
+function loadUsers(renderInfo) {
+    $(renderInfo.users).load(
+        "https://6552de2f5449cfda0f2de0bd.mockapi.io/session/1/users",
+        function (response, status, xhr) {
+            // to show user required is in progress
+            $(renderInfo.loader).detach();
+            // fill the dom with users data
+            $(renderInfo.users).html(fillUserDetails(JSON.parse(response)));
+            // to reduce flicker
+            $(renderInfo.userContainer).removeClass("invisible");
+        },
+    );
+}
