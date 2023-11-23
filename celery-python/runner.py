ba41f97b7771874ff067d89afebbf4ac99bebe76
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2023-11-23 23:21:03 +0545

    celery requirements and task created

--- celery-python/runner.py
+++ celery-python/runner.py
@@ -1,5 +1,15 @@
-from yaaredis import StrictRedis
 from celery import Celery
+from time import sleep
 
 
-cache = StrictRedis(host="localhost", port=6379, db=0)
+#  ** remaining work (celery -A runner inspect active)
+#  ** start-worker (celery -A runner worker --loglevel=info)
+
+
+# cache = StrictRedis(host="localhost", port=6379, db=0)
+app = Celery('tasks', broker='redis://localhost:6379/0', backend='rpc://')
+
+@app.task
+def hello(task_id):
+    sleep(5)
+    print(f'hello world {task_id}')
