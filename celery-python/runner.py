cad5c8fb524e4ead5dca697196cbac6a33c88c5e
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2023-11-24 22:52:49 +0545

    non decorator syntax for registering tasks celery

--- celery-python/runner.py
+++ celery-python/runner.py
@@ -9,7 +9,14 @@ from time import sleep
 # cache = StrictRedis(host="localhost", port=6379, db=0)
 app = Celery('tasks', broker='redis://localhost:6379/0', backend='rpc://')
 # app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/1')
+#  remaining works len(app.control.inspect().reserved().get('celery@Mac.local'))
 
-@app.task
+#@app.task
 def hello(task_id):
+    sleep(2)
     print(f'hello world {task_id}')
+
+# create task without decorator
+
+hello = app.task(hello)
+app.tasks.register(hello)
