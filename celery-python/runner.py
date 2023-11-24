b3f6035f74d2ef1a3b6fc4d90d9d5d0d312c2454
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2023-11-24 17:53:29 +0545

    reselt return location

--- celery-python/runner.py
+++ celery-python/runner.py
@@ -8,8 +8,8 @@ from time import sleep
 
 # cache = StrictRedis(host="localhost", port=6379, db=0)
 app = Celery('tasks', broker='redis://localhost:6379/0', backend='rpc://')
+# app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/1')
 
 @app.task
 def hello(task_id):
-    sleep(5)
     print(f'hello world {task_id}')
