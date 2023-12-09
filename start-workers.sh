c568db1320d3237691086c51965f59996eda27d9
Lekhnath Parajuli <107562350+lekhnath-parajuli@users.noreply.github.com>
2023-12-09 15:52:15 +0545

    Update start-workers.sh
    _

--- start-workers.sh
+++ start-workers.sh
@@ -4,5 +4,5 @@ celery -A background worker --loglevel=info -Q hi &
 celery -A background worker --loglevel=info -Q bye &
 celery -A background worker --loglevel=info -Q hello &
 celery -A background worker --loglevel=info -Q welcome &
-# to loc the terminal so that docker can continuously look into logs asynchronously
+# to lock the terminal so that docker can continuously look into logs asynchronously
 tail -f ${tty}
