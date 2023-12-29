d9f38922c4b4b5649c930168ef6dbd6946d1ae22
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2023-12-29 10:56:54 +0545

    requirement and config updated

--- celery_config.py
+++ celery_config.py
@@ -1,7 +1,15 @@
 from kombu import Exchange, Queue
 
 
-CELERY_QUEUES = (
+worker_pool = "solo"
+broker_heartbeat = 10
+redbeat_lock_timeout = 12 * 30 * 24 * 60 * 60
+beat_max_loop_interval = 10
+broker_connection_retry = 120
+broker_connection_retry_on_startup = None
+broker_connection_max_retries = None
+
+task_queues = (
     Queue("hi", Exchange("hi"), routing_key="hi"),
     Queue("bye", Exchange("bye"), routing_key="bye"),
     Queue("hello", Exchange("hello"), routing_key="hello"),
